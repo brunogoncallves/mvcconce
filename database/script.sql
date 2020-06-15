35e976e94553672b605bfcc8fc666b84439744d2
brunogoncallves <alexbrunogb50@hotmail.com>
2020-06-15 15:02:15 -0300

    :construction: script

---
+++ database/script.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE concessionaria DEFAULT CHARACTER SET utf8;
+USE concessionaria;
+
+CREATE TABLE Motores (
+    MotorID INTEGER NOT NULL AUTO INCREMENT PRIMARY KEY,
+    potencia VARCHAR (3),
+    qntCilindros INTEGER
+);
+CREATE TABLE carros (
+    chassi INTEGER NOT NULL AUTO INCREMENT PRIMARY KEY,
+    marca VARCHAR (255),
+    modelo VARCHAR (255),
+    COR VARCHAR (255),
+    motor INTEGER NOT NULL,
+    FOREIGN KEY (MotorID) REFERENCES Motores(MotorID)
+);
